07283a086b745b2e1e32b7b0527dbdbab1e2320f
Ofir Iluz <ofir.iluz@cyberark.com>
2022-02-24 19:56:54 +0200

    schemas

---
+++ schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Skill(BaseModel):
+    skill_name: str
+    skill_level: int
+
+
+class Person(BaseModel):
+    name: str
+    skills: List[Skill]
+
+
+class Project(BaseModel):
+    name: str
+    num_time_to_finish: int
+    score_on_finish: int
+    best_before: int
+    skills: List[Skill]
+
+
+class ParsedData(BaseModel):
+    persons: List[Person]
+    projects: List[Project]
